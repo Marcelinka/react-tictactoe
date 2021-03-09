5d8e407613c12a2a44c248611e8a7030b65c9928
Marcelinka <miss.knigomanka@gmail.com>
2021-03-09 15:14:48 +0300

    add saving players names

--- src/index.js
+++ src/index.js
@@ -12,16 +12,56 @@ class App extends React.Component {
       firstPlayer: "",
       secondPlayer: ""
     };
+
+    this.tempFirstPlayer = "";
+    this.tempSecondPlayer = "";
+  }
+
+  changeFirstPlayer = e => {
+    this.tempFirstPlayer = e.target.value;
+  };
+
+  changeSecondPlayer = e => {
+    this.tempSecondPlayer = e.target.value;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState(() => ({
+      firstPlayer: this.tempFirstPlayer,
+      secondPlayer: this.tempSecondPlayer
+    }));
   }
 
   render() {
+    const players = this.state.firstPlayer && this.state.secondPlayer && (
+      <div>
+        <p>
+          Player <b>X</b>: {this.state.firstPlayer}
+        </p>
+        <p>
+          Player <b>O</b>: {this.state.secondPlayer}
+        </p>
+      </div>
+    );
+
     return (
       <div>
-        <form className="name-form" onSubmit={e => e.preventDefault()}>
-          <input name="first" placeholder="Name of first player" />
-          <input name="second" placeholder="Name of second player" />
-          <button>Save</button>
+        <form className="name-form" onSubmit={this.handleSubmit}>
+          <input
+            name="first"
+            placeholder="Name of first player"
+            onChange={this.changeFirstPlayer}
+          />
+          <input
+            name="second"
+            placeholder="Name of second player"
+            onChange={this.changeSecondPlayer}
+          />
+          <button type="submit">Save</button>
         </form>
+
+        {players}
 
         <Game />
       </div>
