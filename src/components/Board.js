0d59be304aede94dd599edb4b8717d90da2143cf
Marcelinka <miss.knigomanka@gmail.com>
2021-03-09 14:39:29 +0300

    1 file modified: add keys for rendering

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,7 @@ export default class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i].value}
         onClick={() => this.props.onClick(i)}
         isHighlighted={this.props.squares[i].highlighted}
@@ -15,12 +16,16 @@ export default class Board extends React.Component {
 
   render() {
     let tableCell = -1;
-    const table = new Array(3).fill(undefined).map(() => {
+    const table = new Array(3).fill(undefined).map((el, i) => {
       const tableRow = new Array(3).fill(undefined).map(() => {
         tableCell++;
         return this.renderSquare(tableCell);
       });
-      return <div className="board-row">{tableRow}</div>;
+      return (
+        <div className="board-row" key={i}>
+          {tableRow}
+        </div>
+      );
     });
 
     return <div>{table}</div>;
